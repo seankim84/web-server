b9a454bfe97e200b87bccd505ab17020c2fcb1e2
SeanKim <sean.kim84@gmail.com>
2019-11-05 19:35:46 +0900

    Wiring up User interface

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
-
 //callback 이란 자기자신을 한번더 호출하는 것을 의미한다
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoic2VhbmtpbSIsImEiOiJjazJnMHl6MW8wcTY0M2xvMDVlOHd3MzVsIn0.v2I7Y3x5djsMa_39oCUPJw`
@@ -12,7 +11,7 @@ const geocode = (address, callback) => {
     }) => {
         if (error) {
             callback('Unable connect to location System', undefined)
-        } else if (body.features.length === 0) {
+        } else if (body.features.length == 0) {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, {
