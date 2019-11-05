11b19a6cceaa68f381abf2391a36e68859433ef7
SeanKim <sean.kim84@gmail.com>
2019-11-05 17:01:29 +0900

    Adding  a deafult value

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/432750072e7cd4e174fae944e7e96bad/${latitude},${longitude}`
     request({ url, json: true }, (error, { body }) => {
-        console.log(body)
+        
         if (error) {
             callback('Unable open this file', undefined)
         } else {
