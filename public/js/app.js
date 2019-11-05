40a94994033f995eb4c98ba054abf25e4c66cc43
SeanKim <sean.kim84@gmail.com>
2019-11-05 17:10:06 +0900

    fetch the data

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+console.log('Client side javscript file is loaded');
+
+fetch('http://puzzle.mead.io/puzzle')
+.then((response) => {
+    response.json()
+    .then(data => {
+        console.log(data);
+    })
+})
+
+fetch("http://localhost:3000/weather?address=boston")
+.then(response => {
+    response.json()
+    .then(data  => console.log(data));
+})
